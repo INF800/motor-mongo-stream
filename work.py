99e3e949e515b49d669125103751b70d5f33dc87
Asapanna Rakesh <45640029+INF800@users.noreply.github.com>
2022-01-18 09:05:53 +0530

    Update work.py

--- work.py
+++ work.py
@@ -4,7 +4,9 @@ import motor.motor_asyncio
 
 async def main_async():
     # client = motor.motor_asyncio.AsyncIOMotorClient('172.16.238.10', replicaset='rs0') # connect via internal IP
-    client = motor.motor_asyncio.AsyncIOMotorClient('0.0.0.0', port=27018, replicaset='rs0') # connect via external IP (note the port)
+    # client = motor.motor_asyncio.AsyncIOMotorClient('0.0.0.0', port=27018, replicaset='rs0') # connect via external IP (note the port)
+    client = motor.motor_asyncio.AsyncIOMotorClient('143.198.72.16', port=27018, replicaset='rs0') # connect via external IP (note the port)
+
     db = client.test_database
     count = 0
     while count < 100:
